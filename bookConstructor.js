c18cbc55fa53f95a734fc746e7d3af48cb19c1aa
gerijeb <gtpepi@hotmail.com>
2022-01-25 11:53:05 -0300

    Update CSS, HTML to include form, update JS with querySelectors

--- bookConstructor.js
+++ bookConstructor.js
@@ -1,22 +1,23 @@
- // object constructor 
-// If you’re using constructors to make your 
-// objects it is best to define functions 
-// on the prototype of that object
+addBtn = document.querySelector(".addBtn");
+deleteBtn = document.querySelector(".deleteBtn");
+statsBtn = document.querySelector(".statsBtn");
+tittleInpt = document.querySelector("#tittle");
+authorInpt = document.querySelector("#Author");
+pagesInpt = document.querySelector("#pages");
+readInpt = document.querySelector("#read");
+
 function Book (tittle, author, pages, read) {
     this.tittle = tittle; 
     this.author = author; 
     this.pages = pages;
     this.read = read;
 }
-// info on the prototype so it doesn't make a
-// copy of the function with every inheritance
 Book.prototype.info = function () {
     let readStatus;
     this.read? readStatus = "already read." : readStatus = "not read yet.";
     return `${this.tittle}, by ${this.author}, ${this.pages} pages, ${readStatus}`;
 }
-const book1 = new Book ('The Fellowship of the Ring', 'J. R. R. Tolkien', 423, false);
-
+//const book1 = new Book ('The Fellowship of the Ring', 'J. R. R. Tolkien', 423, false);
 let myLibrary = [];
 
 
