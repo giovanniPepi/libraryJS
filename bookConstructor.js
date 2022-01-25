f6816ed7ca0c910349673445cba03930f41e4499
gerijeb <gtpepi@hotmail.com>
2022-01-25 12:49:39 -0300

    Add querySelectors, EventListeners and getUserInput

--- bookConstructor.js
+++ bookConstructor.js
@@ -1,11 +1,25 @@
-addBtn = document.querySelector(".addBtn");
-deleteBtn = document.querySelector(".deleteBtn");
-statsBtn = document.querySelector(".statsBtn");
-tittleInpt = document.querySelector("#tittle");
-authorInpt = document.querySelector("#Author");
-pagesInpt = document.querySelector("#pages");
-readInpt = document.querySelector("#read");
+let myLibrary = [];
 
+enableQuerySelectors = () => {
+    addBtn = document.querySelector(".addBtn");
+    deleteBtn = document.querySelector(".deleteBtn");
+    statsBtn = document.querySelector(".statsBtn");
+    tittleInpt = document.querySelector("#tittle");
+    authorInpt = document.querySelector("#Author");
+    pagesInpt = document.querySelector("#pages");
+    readInpt = document.querySelector("#read");
+}
+enableEventListeners = () => {
+    addBtn.addEventListener("click", getUserInpt);
+    statsBtn.addEventListener("click", () => console.table(myLibrary));
+}
+getUserInpt = () => {
+    const tittle = tittleInpt.value;
+    const author = authorInpt.value;
+    const pages = pagesInpt.value;
+    const read = readInpt.value;
+    return myLibrary.push(new Book (tittle, author, pages, read));
+}
 function Book (tittle, author, pages, read) {
     this.tittle = tittle; 
     this.author = author; 
@@ -18,6 +32,5 @@ Book.prototype.info = function () {
     return `${this.tittle}, by ${this.author}, ${this.pages} pages, ${readStatus}`;
 }
 //const book1 = new Book ('The Fellowship of the Ring', 'J. R. R. Tolkien', 423, false);
-let myLibrary = [];
-
-
+enableQuerySelectors();
+enableEventListeners();
